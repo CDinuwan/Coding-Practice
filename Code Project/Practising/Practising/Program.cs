47f67d6bd414606c16db55baa047c9366da8e5f1
CDinuwan <chanukadinuwan35@gmail.com>
2020-04-16 03:22:48 +0530

    fibbonacci number

---
+++ Code Project/Practising/Practising/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practising
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter value for n number : ");
+            int n = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value for k number : ");
+            int k = Int32.Parse(Console.ReadLine());
+
+            int num = 1;
+            int fac=1;
+            do
+            {
+                num = num * n;
+                n--;
+            } while (n > 0);//Factorial Getting in do while loop
+
+            do
+            {
+                fac = fac * k;
+                n--;
+            } while (n > 0);
+            Console.WriteLine(num/fac);
+
+            Console.ReadLine();
+        }
+    }
+}
