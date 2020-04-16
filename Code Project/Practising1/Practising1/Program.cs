2462ba45d91af6f196071fc10e1687aba434be33
CDinuwan <chanukadinuwan35@gmail.com>
2020-04-16 15:02:18 +0530

    Try to manipulate array

---
+++ Code Project/Practising1/Practising1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practising1
+{
+    class Program
+    {
+        static void binarySearch(int[] arr,int length)
+        {
+            
+            for(int i=0;i<length;i++)
+            {
+                int val = arr[i];
+                int num = arr[i++];
+                int count = 1;
+                if(val==num)
+                {
+                    count++;
+                    Console.WriteLine(val);
+                    break;
+                }
+
+            }
+            
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] sortNum = { 1, 1, 3, 4, 5, 6, 7, 8 };
+            int arrayLengh = sortNum.Length;
+
+            binarySearch(sortNum, arrayLengh);
+
+            Console.ReadLine();
+        }
+    }
+}
