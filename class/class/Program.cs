c44fdb1fd96ebbd09c669a78b95edeee988d9ea0
CDinuwan <chanukadinuwan35@gmail.com>
2020-05-18 20:09:26 +0530

    Practise C# Classes and constructor

---
+++ class/class/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @class
+{
+    class Car//Creating class
+    {
+
+        public string make;
+        public string color;
+
+        public void StartDrivingCar()
+        {
+            Console.WriteLine("We are going to ride a Car");
+        }
+        public Car(string Color, string Make)
+        {
+            this.color = Color;
+            this.make = Make;
+        }
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car("Blue","Honda");//Constructor
+
+            Console.WriteLine("My car color is {0} and It's {1}", mycar.color, mycar.make);
+
+            float val=DeterminemarketValue(mycar);
+            Console.WriteLine("{0:C}", val);//Format
+
+            Console.ReadLine();
+        }
+
+        private static float DeterminemarketValue(Car car)
+        {
+            float carValue = 100;
+            return carValue;
+            
+        }
+    }
+}
