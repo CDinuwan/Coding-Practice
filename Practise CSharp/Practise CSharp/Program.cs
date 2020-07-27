a67ca4612b0cc98b8dcefa23acade57715225caa
CDinuwan <chanukadinuwan35@gmail.com>
2020-07-27 21:33:01 +0530

    Restart

---
+++ Practise CSharp/Practise CSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practise_CSharp
+{
+    class Car
+    {
+        private string color;
+        public Car(string color)
+        {
+            this.color = color;
+        }
+        Car car = new Car("Red");
+
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? num1 = null;
+            Console.WriteLine(num1);
+            num1 = 1;
+            Console.WriteLine(num1);
+            List<string> listOfName = new List<string>()
+        {
+            "Chanuka Dinuwan",
+            "Dinuwan",
+            "Jan Doe"
+        };
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(listOfName[i]);
+            }
+        }
+
+        enum Days { Sun, Mon, Tue, Wed, Thu };
+    }
+}
