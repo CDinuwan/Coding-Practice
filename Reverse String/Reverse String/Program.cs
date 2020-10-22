57125082382b0ae2205c3b1e4bc0a313217cfac0
CDinuwan <chanukadinuwan35@gmail.com>
2020-10-22 18:17:58 +0530

    Reverse string

---
+++ Reverse String/Reverse String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your string?");
+            string reverse = Console.ReadLine();
+            ReverseString(reverse);
+            
+        }
+        public static void ReverseString(string message)
+        {
+            char[] messageArr=message.ToCharArray();
+            Array.Reverse(messageArr);
+            Console.Write("Result : ");
+            foreach (var item in messageArr)
+            {
+                Console.Write(item);
+            }
+            
+        }
+    }
+    
+}
