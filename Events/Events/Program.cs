c4370d8191f1b5b2c69fad8be1979a6d509665a9
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-04 20:28:13 +0530

    Event practise

---
+++ Events/Events/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class Person
+    {
+        private string _name;
+        private ClockTower _tower;
+        public Person(string name,ClockTower tower)
+        {
+            _name = name;
+            _tower = tower;
+        }
+    }
+    public class ClockTower
+    {
+        public event ChimeEventHandler Chime;
+        
+        public void ChimeFivePM()
+        {
+            Chime();
+        }
+        public void chimeSixAM()
+        {
+            Chime();
+        }
+    }
+
+    public delegate void ChimeEventHandler();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Static class/Static class/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(144));
+            
+            Console.ReadLine();
+        }
+    }
+}
